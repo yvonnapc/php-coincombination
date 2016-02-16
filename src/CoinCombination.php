5cd229ef972d3469d026515d1e298632d83f53d5
Jason Wuerch and Yvonna Contreras <wuerchjason@gmail.com>
2016-02-16 11:50:16 -0800

    Add first function

--- src/CoinCombination.php
+++ src/CoinCombination.php
@@ -1,15 +1,14 @@
 <?php
     class CoinCombination
     {
-        function makeCombination($change)
+        function makeCombination()
         {
-
           $quarters = 0;
           $dimes = 0;
           $nickels = 0;
           $pennies = 0;
 
-          $amount = ['']
+          $amount = [''];
         }
 
     }

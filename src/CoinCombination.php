00d571afb5045f471e98baecf5d62af4fd233781
Jason Wuerch and Yvonna Contreras <student@epicodus.com>
2016-02-16 11:31:00 -0800

    changes

--- src/CoinCombination.php
+++ src/CoinCombination.php
@@ -4,6 +4,12 @@
         function makeCombination($change)
         {
 
+          $quarters = 0;
+          $dimes = 0;
+          $nickels = 0;
+          $pennies = 0;
+
+          $amount = ['']
         }
 
     }

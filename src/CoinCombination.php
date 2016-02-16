0710106614fa55c40929fd114a530736be31a751
Jason Wuerch and Yvonna Contreras <wuerchjason@gmail.com>
2016-02-16 12:39:33 -0800

    Add coins functions

--- src/CoinCombination.php
+++ src/CoinCombination.php
@@ -1,14 +1,21 @@
 <?php
     class CoinCombination
     {
-        function makeCombination()
+        function makeCombination($cents)
         {
-          $quarters = 0;
-          $dimes = 0;
-          $nickels = 0;
-          $pennies = 0;
+          $coins = array(
+            'quarters' => 0,
+            'dimes' => 0,
+            'nickels' => 0,
+            'pennies' => 0,
+          );
+          $coins["quarters"] = (int)($cents % 25);
 
-          $amount = [''];
+          $coins["dimes"] = (int)($cents / 10);
+
+          $coins["nickels"] = ($cents / 5);
+
+          $coins["pennies"] = ($cents / 1)
         }
 
     }
